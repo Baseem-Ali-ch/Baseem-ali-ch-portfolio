69267059b32352a7f0fc30f684421e4c621fa660
Baseem ali ch <baseemch43@gmail.com>
2025-06-15 21:41:31 +0530

    update the logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Baseem Ali C H",
+  keywords: ["baseem", "ali", "c h", "baseem ali c h"],
+  description: "Portfolio of Baseem Ali C H",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
